0a11c0b4db48d037152e2274156556c7ed63e7d6
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2025-11-29 17:07:57 +0000

    feat: user Creat

---
+++ src/main/java/com/example/myplanner/entities/User.java
@@ -0,0 +1,14 @@
+package com.example.myplanner.entities;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
